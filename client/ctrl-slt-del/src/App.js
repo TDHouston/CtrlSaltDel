9c71d31170d5a527c3374e784e0259bf9b02e81b
Olivia <oliviatekolste@gmail.com>
2025-07-21 14:04:09 -0400

    set up routing

--- client/ctrl-slt-del/src/App.js
+++ client/ctrl-slt-del/src/App.js
@@ -1,18 +1,31 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Nav from './components/Nav'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Nav from "./components/Nav";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import Recipe from "./pages/Recipe";
+import Explore from "./pages/Explore";
+import AboutUs from "./pages/AboutUs";
+import RecipeForm from "./pages/RecipeForm";
+import Error from "./pages/Error";
 import Home from "./pages/Home";
-import RecipeCard from "./components/RecipeCard";
 
 function App() {
   return (
-     <Router>
-      <Nav/>
-        <Routes>
-          <Route path="/" element=<Home /> />
-         </Routes>
-      </Router>
+    <Router>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile/:id" element={<Profile />} />
+        <Route path="/recipe/:id" element={<Recipe />} />
+        <Route path="/explore" element={<Explore />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/form:id" element={<RecipeForm />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router>
   );
+}
 
 export default App;
-  
