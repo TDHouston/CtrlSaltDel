67bbdfbd1646ac683751a8727dfbc1ade490ccc3
hoa-dao <hoadao824@gmail.com>
2025-07-21 09:40:37 -0600

    Compeleted Recipe, Instruction, Ingredient, and Recipe_Ingredient models, along w/ Difficulty, Role, and Unit enums.

--- server/src/main/java/capstone/round_table/models/Recipe.java
+++ server/src/main/java/capstone/round_table/models/Recipe.java
@@ -1,5 +1,90 @@
 package capstone.round_table.models;
 
 public class Recipe {
+    private int recipeId;
     private int userId;
+    private int categoryId;
+    private String name;
+    private Difficulty difficulty;
+    private int cookTime;
+    private int servings;
+    private String description;
+    private int upvotes;
+
+    /**
+     * difficulty, cook_time, servings, description, upvotes values aren't required
+     */
+    public Recipe(){}
+
+    public int getRecipeId() {
+        return recipeId;
+    }
+
+    public void setRecipeId(int recipeId) {
+        this.recipeId = recipeId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public int getCookTime() {
+        return cookTime;
+    }
+
+    public void setCookTime(int cookTime) {
+        this.cookTime = cookTime;
+    }
+
+    public int getServings() {
+        return servings;
+    }
+
+    public void setServings(int servings) {
+        this.servings = servings;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getUpvotes() {
+        return upvotes;
+    }
+
+    public void setUpvotes(int upvotes) {
+        this.upvotes = upvotes;
+    }
 }
--- server/src/main/java/capstone/round_table/models/Unit.java
+++ server/src/main/java/capstone/round_table/models/Unit.java
@@ -1,5 +1,28 @@
 package capstone.round_table.models;
 
 public enum Unit {
-    
+    TEASPOON("tsp"),
+    TABLESPOON("tbsp"),
+    CUP("cup"),
+    FLUID_OUNCE("fl oz"),
+    PINT("pt"),
+    QUART("qt"),
+    GALLON("gal"),
+    OUNCE("oz"),
+    POUND("lb"),
+    MILLILITER("mL"),
+    LITER("L"),
+    GRAM("g"),
+    KILOGRAM("kg"),
+    PINCH("pinch");
+
+    private String abbreviation;
+
+    Unit(String abbreviation) {
+        this.abbreviation = abbreviation;
+    }
+
+    public String getAbbreviation() {
+        return abbreviation;
+    }
 }
