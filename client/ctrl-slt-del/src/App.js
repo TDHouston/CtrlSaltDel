27729e7e5fcf2fdec0ca42ef9682de9d06bfc96a
Julian Houston <89494245+TDHouston@users.noreply.github.com>
2025-07-21 14:20:38 -0400

    added footer

--- client/ctrl-slt-del/src/App.js
+++ client/ctrl-slt-del/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav";
 import Home from "./pages/Home";
-import RecipeCard from "./components/RecipeCard";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -11,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
