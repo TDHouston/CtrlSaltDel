a1a0941c4e4705e8fe36bf2a133a49dac8b93746
Jason Wu <jsonwu34@gmail.com>
2025-07-22 12:35:47 -0400

    implemented user service

--- server/src/main/java/capstone/round_table/domain/UserService.java
+++ server/src/main/java/capstone/round_table/domain/UserService.java
@@ -16,35 +16,100 @@ public class UserService {
 
 
     public List<User> findAll() {
-        return List.of();
+        return userRepository.findAll();
     }
 
-    public Result<User> findByEmail(String email) {
-        return null;
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email);
     }
 
-    public Result<User> findById(int id) {
-        return null;
+    public User findById(int userId) {
+        return userRepository.findById(userId);
     }
 
-    public Result<User> findByUsername(String username) {
-        return null;
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
     }
 
     public Result<User> addUser(User user) {
-        return null;
+        Result<User> result = validate(user);
+        if (!result.isSuccess()) {
+            return result;
+        }
+
+        if (user.getUserId() != 0) {
+            result.addMessage("User Id cannot be set", ResultType.INVALID);
+            return result;
+        }
+
+        User addedUser = userRepository.addUser(user);
+        result.setPayload(addedUser);
+
+        return result;
     }
 
     public Result<User> updateUser(User user) {
-        return null;
+        Result<User> result = validate(user);
+        if (!result.isSuccess()) {
+            return result;
+        }
+
+        if (user.getUserId() == 0) {
+            result.addMessage("User Id must be set", ResultType.INVALID);
+            return result;
+        }
+        if (!userRepository.updateUser(user)) {
+            String msg = String.format("user %s, not found", user.getUsername());
+            result.addMessage(msg, ResultType.NOT_FOUND);
+            return result;
+        }
+        
+        return result;
     }
 
-    public Result<Void> deleteUser(int userId) {
-        return null;
+    public boolean deleteUser(int userId) {
+        return userRepository.deleteUser(userId);
     }
 
     private Result<User> validate(User user) {
-        return null;
+        Result<User> userResult = new Result<>();
+
+        if (user == null) {
+            userResult.addMessage("User cannot be null", ResultType.INVALID);
+            return userResult;
+        }
+
+        if (user.getUsername().isEmpty() || user.getUsername().isBlank()) {
+            userResult.addMessage("Username is required", ResultType.MISSING_INFO);
+            return userResult;
+        }
+
+        if (user.getEmail().isEmpty() || user.getEmail().isBlank()) {
+            userResult.addMessage("Email is required", ResultType.MISSING_INFO);
+            return userResult;
+        }
+
+        if (user.getPassword().isEmpty() || user.getPassword().isBlank()) {
+            userResult.addMessage("Password is required", ResultType.MISSING_INFO);
+            return userResult;
+        }
+
+        if (user.getFirstName().isEmpty() || user.getFirstName().isBlank()) {
+            userResult.addMessage("First name is required", ResultType.MISSING_INFO);
+            return userResult;
+        }
+
+        if (user.getLastName().isEmpty() || user.getLastName().isBlank()) {
+            userResult.addMessage("Last name is required", ResultType.MISSING_INFO);
+            return userResult;
+        }
+
+        if (user.getRole() == null) {
+            userResult.addMessage("Role cannot be null", ResultType.INVALID);
+            return userResult;
+        }
+
+        return userResult;
     }
 
 
