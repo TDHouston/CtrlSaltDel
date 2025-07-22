3046a52277a5b7b7a0c6da62db252f929faf4490
Jason Wu <jsonwu34@gmail.com>
2025-07-22 17:49:54 -0400

    setup security currently permit all

--- server/src/main/java/capstone/round_table/controllers/UserController.java
+++ server/src/main/java/capstone/round_table/controllers/UserController.java
@@ -1,6 +1,9 @@
 package capstone.round_table.controllers;
 
+import capstone.round_table.domain.Result;
+import capstone.round_table.domain.UserService;
 import capstone.round_table.models.User;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,16 +14,39 @@ import java.util.List;
 @RequestMapping("/api/user")
 public class UserController {
 
-    // TODO: add reference to service and create controller
+    private final UserService service;
+
+    public UserController(UserService service) {
+        this.service = service;
+    }
 
     @GetMapping
     public List<User> findAll() {
-        return null;
+        return service.findAll();
     }
 
-    @GetMapping("/{userEmail}")
-    public User findByEmail(@PathVariable String userEmail) {
-        return null;
+//    @GetMapping("/{userEmail}")
+//    public User findByEmail(@PathVariable String userEmail) {
+//        return service.findByEmail(userEmail);
+//    }
+//
+//    @GetMapping("/{userEmail}")
+//    public User findByUsername(@PathVariable String username) {
+//        return service.findByUsername(username);
+//    }
+
+    @GetMapping("/hi")
+    public String hi(){
+        return "hi";
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> add(@RequestBody User user) {
+        Result<User> result = service.addUser(user);
+        if (result.isSuccess()) {
+            return new ResponseEntity<>(result.getPayload(), HttpStatus.CREATED);
+        }
+        return ErrorResponse.build(result);
     }
 
     @PutMapping("/{userId}")
