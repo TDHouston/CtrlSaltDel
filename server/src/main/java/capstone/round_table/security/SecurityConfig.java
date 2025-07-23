f99f2c377864fcb8d32fca33f903b182def036a1
Jason Wu <jsonwu34@gmail.com>
2025-07-22 21:22:35 -0400

    cor configure and security configure

--- server/src/main/java/capstone/round_table/security/SecurityConfig.java
+++ server/src/main/java/capstone/round_table/security/SecurityConfig.java
@@ -2,11 +2,13 @@ package capstone.round_table.security;
 
 import capstone.round_table.security.jwt.JwtConverter;
 
+import capstone.round_table.security.jwt.JwtRequestFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -25,7 +27,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.csrf().disable();
         http.cors();
-        http.authorizeRequests().anyRequest().permitAll();
+        http.authorizeRequests()
+                .anyRequest()
+                .permitAll()
+                .and()
+                .addFilter(new JwtRequestFilter(authenticationManager(), converter)) // 3
+                .sessionManagement() // 4
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);;
     }
 
     @Override
