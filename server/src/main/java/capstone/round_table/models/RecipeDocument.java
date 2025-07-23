4cbe3273c3e31d6794e5edef1865a5925e738890
Jason Wu <jsonwu34@gmail.com>
2025-07-23 14:05:36 -0400

    recipe document

--- server/src/main/java/capstone/round_table/models/RecipeDocument.java
+++ server/src/main/java/capstone/round_table/models/RecipeDocument.java
@@ -1,8 +1,31 @@
 package capstone.round_table.models;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
 
 @Document(indexName = "recipes")
 public class RecipeDocument {
+    @Id
+    private int recipeId;
 
+    @Field(type = FieldType.Text, analyzer = "standard")
+    private String name;
+
+    public int getRecipeId() {
+        return recipeId;
+    }
+
+    public void setRecipeId(int recipeId) {
+        this.recipeId = recipeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
