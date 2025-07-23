1428c4430edaf3a34bb021f0bde10b256dadabab
hoa-dao <hoadao824@gmail.com>
2025-07-23 02:32:10 -0600

    Completed Ingredient Controller.

--- server/src/main/java/capstone/round_table/controllers/IngredientController.java
+++ server/src/main/java/capstone/round_table/controllers/IngredientController.java
@@ -1,6 +1,9 @@
 package capstone.round_table.controllers;
 
+import capstone.round_table.domain.IngredientService;
+import capstone.round_table.domain.Result;
 import capstone.round_table.models.Ingredient;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,27 +13,53 @@ import java.util.List;
 @CrossOrigin(origins = {"http://localhost:3000"})
 @RequestMapping("/api/ingredients")
 public class IngredientController {
+    private final IngredientService service;
 
-    // TODO: declare service and make constructor
+    public IngredientController(IngredientService service) {
+        this.service = service;
+    }
 
     @GetMapping
     public List<Ingredient> findAll(){
-        return null;
+        return service.findAll();
+    }
+
+    @GetMapping("/{ingredientId}")
+    public Ingredient findById(@PathVariable int ingredientId) {
+        return service.findById(ingredientId);
     }
 
     @PostMapping
     public ResponseEntity<Object> add(@RequestBody Ingredient ingredient) {
-        return null;
+        Result<Ingredient> result = service.addIngredient(ingredient);
+        if (result.isSuccess()) {
+            return new ResponseEntity<>(result.getPayload(), HttpStatus.CREATED);
+        }
+        return ErrorResponse.build(result);
     }
 
     @PutMapping("/{ingredientId}")
-    public ResponseEntity<Object> update(@PathVariable int ingredientId, @RequestBody Ingredient ingredient) {
-        return null;
+    public ResponseEntity<Object> update(
+        @PathVariable int ingredientId,
+        @RequestBody Ingredient ingredient
+    ) {
+        if (ingredientId != ingredient.getIngredientId()) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+
+        Result<Ingredient> result = service.updateIngredient(ingredient);
+        if (result.isSuccess()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return ErrorResponse.build(result);
     }
 
     @DeleteMapping("/{ingredientId}")
-    public ResponseEntity<Void> deleteById(@PathVariable int ingredientId) {
-        return null;
+    public ResponseEntity<Object> deleteById(@PathVariable int ingredientId) {
+        Result<Ingredient> result = service.deleteIngredientById(ingredientId);
+        if (result.isSuccess()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return ErrorResponse.build(result);
     }
-
 }
